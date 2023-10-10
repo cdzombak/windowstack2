2b1a745cc6c857252b42b620e428cef1e58ab2df
Chris Dzombak <cdzombak@curie.tailnet-003a.ts.net>
2023-10-10 14:14:13 -0400

    fix Homebrew formula audit

--- .formula.sh
+++ .formula.sh
@@ -15,7 +15,7 @@ cat << EOF
 # frozen_string_literal: true
 
 # This file was automatically generated. DO NOT EDIT.
-class WindowStack2 < Formula
+class Windowstack2 < Formula
   desc "Keep a log of frontmost macOS window titles in your terminal"
   homepage "https://github.com/cdzombak/windowstack2"
   url "https://github.com/cdzombak/windowstack2/releases/download/v${FORMULA_VERSION_NO_V}/windowstack2-${FORMULA_VERSION_NO_V}-all.tar.gz"
